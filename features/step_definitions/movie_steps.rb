73dc0b7d813a7a525637ad8b891d73b11b973cf7
Dario Cuevas <dario.cuevas@gmail.com>
2013-02-03 01:32:16 +0100

    Add steps and update scenarios for filter BDD.

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,25 @@
 # Add a declarative step here for populating the DB with movies.
+class CreateMovies < ActiveRecord::Migration
+  def up
+    create_table :movies do |t|
+      t.string :title
+      t.string :rating
+      t.string :release_date
+    end
+  end
+end
+
+CreateMovies.up
+
+class Movie < ActiveRecord::Base
+end
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +28,6 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +38,26 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(',').each do |rating|
+    rating = rating.strip
+    
+    if uncheck == "un" then
+        step %Q{I uncheck "ratings_#{rating}"}
+        step %Q{the "ratings_#{rating}" checkbox should not be checked}
+    else
+        step %Q{I check "ratings_#{rating}"}
+        step %Q{the "ratings_#{rating}" checkbox should be checked}
+    end
+  end
 end
+
+Then /^I should see all of the movies$/ do
+  rows = page.all("table#movies tbody tr td[1]").map! {|t| t.text}
+  assert ( rows.size == Movie.all.count )
+end
+
+Then /^I should see no movies$/ do
+  rows = page.all("table#movies tbody tr td[1]").map! {|t| t.text}
+  assert rows.size == 0
+end
+
